f32af5211ab12f709c0fdc707cbc5fa372dd69a4
Alexander_Popovich <alexusav@mail.ru>
2025-09-23 09:55:23 +0000

    First commit. Full files for CA server

---
+++ ca-server/ca-config.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+
+#Checking the run from root
+if [[ "$EUID" -ne 0 ]]; then
+    echo "I need root privileges."
+    exit 1
+fi
+
+apt-get update
+#Install easyrsa
+if sudo dpkg -l easy-rsa 2>&1 | grep ii &>/dev/null
+then
+        echo "Pakage easy-rsa is installed ++++++++++++"
+else
+	if sudo apt-get install -y easy-rsa
+        then
+                echo "Install SUCCESS easy-rsa ========="
+        else
+                echo "Install easy-rsa FAILED! ============"
+                exit 1
+        fi
+fi
+
+
+#set -x
+#Create directory easyrsa
+if ! test -e /var/easy-rsa
+then
+	mkdir /var/easy-rsa
+	chmod +rw /var/easy-rsa
+	echo "Create directory /var/easy-rsa ============"
+	ln -s /usr/share/easy-rsa/* /var/easy-rsa/
+	echo "Create links files /var/easy-rsa ============"
+else
+	echo "The directory /var/easy-rsa exist ============"
+fi
+
+
+#Start init easyrsa
+if test -f /var/easy-rsa/easyrsa
+then
+	cd /var/easy-rsa
+	if bash /var/easy-rsa/easyrsa init-pki 
+	then
+		echo "/var/easy-rsa/pki"
+		if test -d "/var/easy-rsa/pki"
+		then
+			rm "/var/easy-rsa/pki/vars.example"
+			cp "/var/easy-rsa/vars.example" "/var/easy-rsa/pki/vars"
+			echo "EASYRSA init was SUCCESSFUL ============"
+		else
+			echo "ERROR init easyrsa =============="
+		fi
+	else
+		echo "ERROR creation easyrsa ==========="
+		exit 1
+	fi
+else
+	echo "File /var/easy-rsa/easyrsa not found "
+	exit 1
+fi
+
+
+#Edit vars
+if test -f "/var/easy-rsa/pki/vars"
+then
+	sed -i 's/^#*set_var EASYRSA_REQ_COUNTRY.*$/set_var EASYRSA_REQ_COUNTRY     "RUS"/' "/var/easy-rsa/pki/vars"
+	sed -i 's/^#*set_var EASYRSA_REQ_PROVINCE.*$/set_var EASYRSA_REQ_PROVINCE    "Astrakhan region"/' "/var/easy-rsa/pki/vars"
+	sed -i 's/^#*set_var EASYRSA_REQ_CITY.*$/set_var EASYRSA_REQ_CITY        "Astrakhan"/' "/var/easy-rsa/pki/vars"
+	sed -i 's/^#*set_var EASYRSA_REQ_ORG.*$/set_var EASYRSA_REQ_ORG         "MyCompany"/' "/var/easy-rsa/pki/vars"
+	sed -i 's/^#*set_var EASYRSA_REQ_EMAIL.*$/set_var EASYRSA_REQ_EMAIL       "support@mycompany.ru"/' "/var/easy-rsa/pki/vars"
+	sed -i 's/^#*set_var EASYRSA_REQ_OU.*$/set_var EASYRSA_REQ_OU          "DevOps"/' "/var/easy-rsa/pki/vars"
+	sed -i 's/^#*set_var EASYRSA_ALGO.*$/set_var EASYRSA_ALGO            ec/' "/var/easy-rsa/pki/vars"
+	sed -i 's/^#*set_var EASYRSA_DIGEST.*$/set_var EASYRSA_DIGEST          "sha512"/' "/var/easy-rsa/pki/vars"
+	echo "File VARS edit SUCCESFUL ================"
+else
+	echo "ERROR File vars.example not fount"
+	exit 1
+fi
+
+
+
+#Start create build-ca
+if ! test -f "/var/easy-rsa/pki/ca.crt"
+then
+	if bash /var/easy-rsa/easyrsa build-ca
+	then
+		echo "CA creation SUCCESSFUL! ============="
+	else
+		echo "ERROR creation CA ============"
+		exit 1
+	fi
+else
+	echo "The CA.crt file exists ++++++++++"
+fi
+
+#Start create servers crt
+if test -d "/var/easy-rsa/pki"
+then
+	if bash /var/easy-rsa/easyrsa gen-req MyCompany nopass
+	then 
+		echo "Creation req SUCCESSFUL +++++++++++"
+		
+		if bash /var/easy-rsa/easyrsa sign-req server MyCompany
+		then
+			echo "Creation sign SUCCESSFUL ++++++++"
+		else
+			echo "ERROR no sign sert ============"
+			exit 1
+		fi
+	else
+		echo "ERROR no creation req ============="
+		exit 1
+	fi
+else
+	echo "ERROR Directory /var/easy-rsa/pki not found =========="
+fi
+
+#Start creatinon client crt
+if test -d "/var/easy-rsa/pki"
+then
+        if bash /var/easy-rsa/easyrsa gen-req iamclient nopass
+        then
+                echo "Creation req SUCCESSFUL +++++++++++"
+
+                if bash /var/easy-rsa/easyrsa sign-req client iamclient
+                then
+                        echo "Creation sign SUCCESSFUL ++++++++"
+                else
+                        echo "ERROR no sign sert ============"
+                        exit 1
+                fi
+        else
+                echo "ERROR no creation req ============="
+                exit 1
+        fi
+else
+        echo "ERROR Directory /var/easy-rsa/pki not found =========="
+fi
+
+
+
---
+++ first-config.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+#UNINSTALL=(firewalld)
+#prometheus prometheus-alertmanager
+INSTALL=(iptables iptables-persistent prometheus-node-exporter)
+#INSTALL=(firewalld)
+
+#Checking the run from root
+if [[ "$EUID" -ne 0 ]]; then
+    echo "I need root privileges."
+    exit 1
+fi
+
+
+#Uninstall firewall
+if apt-get remove -y firewalld
+then
+	echo "========= Remove firewalld pakage ============"
+fi
+
+
+#apt-get update
+echo "============= Update pakage ============="
+
+#installed pakages
+for pakage in ${INSTALL[@]}
+do
+	#checking installed pakage
+	if dpkg -l $pakage 2>&1 | grep ii &>/dev/null
+	then
+		echo "Pakage $pakage is installed ++++++++++++"
+	else
+		if apt-get install -y $pakage
+		then
+			echo "Install SUCCESS $pakage ========="
+		else
+			echo "Install $pakage FAILED! ============"
+			exit 1
+		fi
+	fi
+done
+
+#configure ssh /etc/ssh/sshd_config
+sed -i 's/^#*PermitRootLogin.*$/PermitRootLogin no/' /etc/ssh/sshd_config
+sed -i 's/^#*Port.*$/Port 2222/' /etc/ssh/sshd_config
+sed -i 's/^#*PubkeyAuthentication.*$/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+sudo service ssh restart
+
+
+#enable iptables
+if systemctl status iptables
+then
+	systemctl enable iptables
+	systemctl start iptables
+fi
+
+#configure iptables
+iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT -m comment --comment dns
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT -m comment --comment dns
+iptables -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p icmp -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 2222 -j ACCEPT -m comment --comment ssh
+iptables -A OUTPUT -p tcp -m multiport --dports 443,80 -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -m state --state INVALID -j DROP
+iptables -A INPUT -m state --state INVALID -j DROP
+iptables -P OUTPUT DROP
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+
+service iptables-persistent save
+
+
+
+
