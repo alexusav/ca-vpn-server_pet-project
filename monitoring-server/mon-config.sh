2c278cfed49d7b075591fae8f00e776a572e7cf1
Alexander_Popovich <alexusav@mail.ru>
2025-10-14 06:50:31 +0000

    Update monitoring-server

--- monitoring-server/mon-config.sh
+++ monitoring-server/mon-config.sh
@@ -39,6 +39,9 @@ iptables -I INPUT -p tcp -s 10.130.0.7 --dport 9100 -j ACCEPT -m comment --comme
 iptables -I OUTPUT -p tcp -d 10.130.0.0/24 --dport 9100 -j ACCEPT -m comment --comment Prometheus_node_exporter
 iptables -I OUTPUT -p tcp -d 10.130.0.0/24 --dport 9176 -j ACCEPT -m comment --comment prometheus_openvpn_exporter
 iptables -I OUTPUT -p tcp -d 10.130.0.0/24 --dport 9113 -j ACCEPT -m comment --comment prometheus_nginx_exporter
+iptables -A INPUT -p tcp --dport 9093 -j ACCEPT -m comment --comment prometheus_alertmanager
+iptables -A OUTPUT -p tcp --dport 587 -j ACCEPT -m comment --comment smtp
+
 
 service netfilter-persistent save
 
